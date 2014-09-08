4ce1368f23d9f966abe491eab1c910e895e240ca
Jonathan Ong <jonathanrichardong@gmail.com>
2014-09-08 13:02:31 -0700

    init

---
+++ index.js
@@ -0,0 +1,66 @@
+
+var assert = require('assert');
+var statuses = require('statuses');
+var inherits = require('util').inherits;
+
+exports = module.exports = function (status, msg, props) {
+  if ('object' == typeof status && !(status instanceof Error)) {
+    props = status;
+    status = null;
+  }
+
+  // create(msg, status)
+  // this should be removed, but remains for koa backwards compat
+  if ('number' == typeof msg) {
+    var tmp = msg;
+    msg = status || statuses[tmp];
+    status = tmp;
+  }
+
+  props = props || {};
+  var err = msg instanceof Error
+    ? msg
+    : new Error(msg || statuses[status || 500]);
+  for (var key in props) err[key] = props[key];
+  err.status = err.statusCode = status || err.status || 500;
+  assert(statuses(err.status), 'invalid status code');
+  err.expose = 'number' === typeof err.status
+    && statuses[err.status]
+    && err.status < 500;
+  return err;
+};
+
+// create generic error objects
+var codes = statuses.codes.filter(function (num) {
+  return num >= 400;
+});
+
+codes.forEach(function (code) {
+  if (code >= 500) {
+    var ServerError = function ServerError(msg) {
+      Error.call(this);
+      this.message = msg || statuses[code];
+      Error.captureStackTrace(this, arguments.callee);
+    }
+    inherits(ServerError, Error);
+    ServerError.prototype.status =
+    ServerError.prototype.statusCode = code;
+    ServerError.prototype.expose = false;
+    exports[code] =
+    exports[statuses[code].replace(/\s+/g, '')] = ServerError;
+    return;
+  }
+
+  var ClientError = function ClientError(msg) {
+    Error.call(this);
+    this.message = msg || statuses[code];
+    Error.captureStackTrace(this, arguments.callee);
+  }
+  inherits(ClientError, Error);
+  ClientError.prototype.status =
+  ClientError.prototype.statusCode = code;
+  ClientError.prototype.expose = false;
+  exports[code] =
+  exports[statuses[code].replace(/\s+/g, '')] = ClientError;
+  return;
+});
---
+++ test/test.js
@@ -0,0 +1,72 @@
+
+var assert = require('assert');
+
+var create = require('..');
+
+describe('HTTP Errors', function () {
+  it('create(status)', function () {
+    var err = create(404);
+    assert.equal(err.message, 'Not Found');
+    assert.equal(err.status, 404);
+    assert.equal(err.statusCode, 404);
+  })
+
+  it('create(status, msg)', function () {
+    var err = create(404, 'LOL');
+    assert.equal(err.message, 'LOL');
+    assert.equal(err.status, 404);
+    assert.equal(err.statusCode, 404);
+  })
+
+  it('create(props)', function () {
+    var err = create({
+      id: 1
+    });
+    assert.equal(err.id, 1);
+    assert.equal(err.message, 'Internal Server Error');
+    assert.equal(err.status, 500);
+    assert.equal(err.statusCode, 500);
+  })
+
+  it('create(msg, status)', function () {
+    var err = create('LOL', 404);
+    assert.equal(err.message, 'LOL');
+    assert.equal(err.status, 404);
+    assert.equal(err.statusCode, 404);
+  })
+
+  it('create(status, err, props)', function () {
+    var _err = new Error('LOL');
+    var err = create(404, _err, {
+      id: 1
+    });
+    assert.equal(err, _err);
+    assert.equal(err.message, 'LOL');
+    assert.equal(err.status, 404);
+    assert.equal(err.statusCode, 404);
+  })
+
+  it('new create.NotFound()', function () {
+    var err = new create.NotFound();
+    assert.equal(err.message, 'Not Found');
+    assert.equal(err.status, 404);
+    assert.equal(err.statusCode, 404);
+    assert(err.stack);
+  })
+
+  it('new create.InternalServerError()', function () {
+    var err = new create.InternalServerError();
+    assert.equal(err.message, 'Internal Server Error');
+    assert.equal(err.status, 500);
+    assert.equal(err.statusCode, 500);
+    assert(err.stack);
+  })
+
+  it('new create["404"]()', function () {
+    var err = new create['404']();
+    assert.equal(err.message, 'Not Found');
+    assert.equal(err.status, 404);
+    assert.equal(err.statusCode, 404);
+    assert(err.stack);
+  })
+})
