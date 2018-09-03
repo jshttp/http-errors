f162b5284cc1b78f15d598cc0692b1426fee07e0
Douglas Christopher Wilson <doug@somethingdoug.com>
2018-09-03 12:03:38 -0400

    tests: use strict assertions

--- test/test.js
+++ test/test.js
@@ -9,53 +9,53 @@ var create = require('..')
 describe('HTTP Errors', function () {
   it('create(status)', function () {
     var err = create(404)
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'Not Found')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
   })
 
   it('create(status) for 300', function () {
     var err = create(300)
-    assert.equal(err.name, 'Error')
-    assert.equal(err.message, 'Multiple Choices')
-    assert.equal(err.status, 300)
-    assert.equal(err.statusCode, 300)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'Multiple Choices')
+    assert.strictEqual(err.status, 300)
+    assert.strictEqual(err.statusCode, 300)
   })
 
   it('create(status) for 471', function () {
     var err = create(471)
-    assert.equal(err.name, 'BadRequestError')
-    assert.equal(err.message, 'Bad Request')
-    assert.equal(err.status, 471)
-    assert.equal(err.statusCode, 471)
+    assert.strictEqual(err.name, 'BadRequestError')
+    assert.strictEqual(err.message, 'Bad Request')
+    assert.strictEqual(err.status, 471)
+    assert.strictEqual(err.statusCode, 471)
   })
 
   it('create(status) for 520', function () {
     var err = create(520)
-    assert.equal(err.name, 'InternalServerError')
-    assert.equal(err.message, 'Internal Server Error')
-    assert.equal(err.status, 520)
-    assert.equal(err.statusCode, 520)
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'Internal Server Error')
+    assert.strictEqual(err.status, 520)
+    assert.strictEqual(err.statusCode, 520)
   })
 
   it('create(status, msg)', function () {
     var err = create(404, 'LOL')
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
   })
 
   it('create(status, props)', function () {
     var err = create(404, {
       id: 1
     })
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'Not Found')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.id, 1)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
   })
 
   it('create(status, props) with status prop', function () {
@@ -63,11 +63,11 @@ describe('HTTP Errors', function () {
       id: 1,
       status: 500
     })
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'Not Found')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.id, 1)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
   })
 
   it('create(status, props) with statusCode prop', function () {
@@ -75,90 +75,90 @@ describe('HTTP Errors', function () {
       id: 1,
       statusCode: 500
     })
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'Not Found')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.id, 1)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
   })
 
   it('create(props)', function () {
     var err = create({
       id: 1
     })
-    assert.equal(err.name, 'InternalServerError')
-    assert.equal(err.message, 'Internal Server Error')
-    assert.equal(err.status, 500)
-    assert.equal(err.statusCode, 500)
-    assert.equal(err.id, 1)
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'Internal Server Error')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.id, 1)
   })
 
   it('create(msg, status)', function () {
     var err = create('LOL', 404)
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
   })
 
   it('create(msg)', function () {
     var err = create('LOL')
-    assert.equal(err.name, 'InternalServerError')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 500)
-    assert.equal(err.statusCode, 500)
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
   })
 
   it('create(msg, props)', function () {
     var err = create('LOL', {
       id: 1
     })
-    assert.equal(err.name, 'InternalServerError')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 500)
-    assert.equal(err.statusCode, 500)
-    assert.equal(err.id, 1)
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.id, 1)
   })
 
   it('create(err)', function () {
     var _err = new Error('LOL')
     _err.status = 404
     var err = create(_err)
-    assert.equal(err, _err)
-    assert.equal(err.name, 'Error')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.expose, true)
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.expose, true)
 
     _err = new Error('LOL')
     err = create(_err)
-    assert.equal(err, _err)
-    assert.equal(err.name, 'Error')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 500)
-    assert.equal(err.statusCode, 500)
-    assert.equal(err.expose, false)
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
 
     err = create(null)
-    assert.notEqual(err, null)
-    assert.equal(err.name, 'InternalServerError')
-    assert.equal(err.message, 'Internal Server Error')
-    assert.equal(err.status, 500)
-    assert.equal(err.statusCode, 500)
-    assert.equal(err.expose, false)
+    assert.notStrictEqual(err, null)
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'Internal Server Error')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
   })
 
   it('create(err) with invalid err.status', function () {
     var _err = new Error('Connection refused')
     _err.status = -1
     var err = create(_err)
-    assert.equal(err, _err)
-    assert.equal(err.name, 'Error')
-    assert.equal(err.message, 'Connection refused')
-    assert.equal(err.status, 500)
-    assert.equal(err.statusCode, 500)
-    assert.equal(err.expose, false)
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'Connection refused')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
   })
 
   it('create(err, props)', function () {
@@ -167,12 +167,12 @@ describe('HTTP Errors', function () {
     var err = create(_err, {
       id: 1
     })
-    assert.equal(err.name, 'Error')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.id, 1)
-    assert.equal(err.expose, true)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
+    assert.strictEqual(err.expose, true)
   })
 
   it('create(status, err, props)', function () {
@@ -180,30 +180,30 @@ describe('HTTP Errors', function () {
     var err = create(404, _err, {
       id: 1
     })
-    assert.equal(err, _err)
-    assert.equal(err.name, 'Error')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.id, 1)
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
   })
 
   it('create(status, msg, props)', function () {
     var err = create(404, 'LOL', {
       id: 1
     })
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'LOL')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.id, 1)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
   })
 
   it('create(status, msg, { expose: false })', function () {
     var err = create(404, 'LOL', {
       expose: false
     })
-    assert.equal(err.expose, false)
+    assert.strictEqual(err.expose, false)
   })
 
   it('new create.HttpError()', function () {
@@ -214,38 +214,38 @@ describe('HTTP Errors', function () {
 
   it('new create.NotFound()', function () {
     var err = new create.NotFound()
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'Not Found')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.expose, true)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.expose, true)
     assert(err.stack)
   })
 
   it('new create.InternalServerError()', function () {
     var err = new create.InternalServerError()
-    assert.equal(err.name, 'InternalServerError')
-    assert.equal(err.message, 'Internal Server Error')
-    assert.equal(err.status, 500)
-    assert.equal(err.statusCode, 500)
-    assert.equal(err.expose, false)
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'Internal Server Error')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
     assert(err.stack)
   })
 
   it('new create["404"]()', function () {
     var err = new create['404']()
-    assert.equal(err.name, 'NotFoundError')
-    assert.equal(err.message, 'Not Found')
-    assert.equal(err.status, 404)
-    assert.equal(err.statusCode, 404)
-    assert.equal(err.expose, true)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.expose, true)
     assert(err.stack)
   })
 
   it('should preserve error [[Class]]', function () {
-    assert.equal(Object.prototype.toString.call(create('LOL')), '[object Error]')
-    assert.equal(Object.prototype.toString.call(new create[404]()), '[object Error]')
-    assert.equal(Object.prototype.toString.call(new create[500]()), '[object Error]')
+    assert.strictEqual(Object.prototype.toString.call(create('LOL')), '[object Error]')
+    assert.strictEqual(Object.prototype.toString.call(new create[404]()), '[object Error]')
+    assert.strictEqual(Object.prototype.toString.call(new create[500]()), '[object Error]')
   })
 
   it('should support err instanceof Error', function () {
